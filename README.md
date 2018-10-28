aa1bac0086f518a4a986081a32c89b2b31ba9c60
Charlie Lee <dev@charlielee.uk>
2018-10-28 23:23:17 +0000

    Add audio stream recorder

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # forgeradioshowrecorder
+
+A small Node program for recording an MP3 stream to a file.
+
+## Usage
+
+Run the following in a terminal window or as a cronjob:
+
+```node
+node index.js -s "http://firewall.pulsradio.com" -d 20000
+```
+
+## Arguments
+
+* `-s` The URL of the stream to record
+* `-d` The duration in milliseconds to record for
---
+++ index.js
@@ -0,0 +1,48 @@
+// Dependencies
+var fs = require('fs');
+var NodeID3 = require('node-id3');
+var argv = require('minimist')(process.argv.slice(2));
+var request = require('request');
+
+// DURATION OF STREAM IN MILLISECONDS
+const DURATION = argv.d || 5000;
+
+// Windows does not allow colons in file names
+var output_file_date = new Date().toISOString().replace(/:/g, "-");
+
+var output_file_name = `shows/show_${output_file_date}.mp3`;
+var output_stream = fs.createWriteStream(output_file_name);
+
+// Streams
+var stream_url = argv.s || 'http://firewall.pulsradio.com';
+var stream = request.get(stream_url);
+
+console.log("Stream:", stream_url,"Duration:", DURATION, "ms");
+console.log("Running audio stream recorder...");
+
+// Counters
+var start_time = null;
+var cur_time = null;
+var cur_length = null;
+
+stream.on("response", function(response) {
+  start_time = Date.now();
+
+  stream.on('data', function(chunk) {
+    // Calculate the time we have been streaming for
+    cur_time = Date.now();
+    cur_length = cur_time - start_time
+
+    // Abort the stream when the duration is passed
+    if (cur_length >= DURATION) {
+      stream.abort();
+      console.log("Finished. Output to:", output_file_name)
+    }
+  })
+  .on('error', function(err) {
+    // handle error
+    console.error(err)
+  });
+
+  stream.pipe(output_stream);
+})
